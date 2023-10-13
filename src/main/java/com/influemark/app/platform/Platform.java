ebda221ce5b3a1de39e87bee787445d7a28697af
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-10-13 21:16:24 +0200

    Platform update

--- src/main/java/com/influemark/app/platform/Platform.java
+++ src/main/java/com/influemark/app/platform/Platform.java
@@ -1,11 +1,15 @@
 package com.influemark.app.platform;
 
 import com.influemark.app.influencer.Influencer;
+import com.influemark.app.post.Post;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,8 +28,13 @@ public class Platform {
     @Column(name = "followers")
     private int numberOfFollowers;
 
+    // Influencer Relationship
     @ManyToOne
     @JoinColumn(name = "influencer_id")
     private Influencer influencer;
+
+    // Post relationship
+    @OneToMany(mappedBy = "platform", cascade = CascadeType.REMOVE)
+    private List<Post> posts = new ArrayList<>();
 
 }
