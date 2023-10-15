402c65704b44e2348d7178e099fba25493c0e5c0
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-10-15 16:35:29 +0200

    updated methods

--- src/main/java/com/influemark/app/security/auth/AuthenticationController.java
+++ src/main/java/com/influemark/app/security/auth/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.influemark.app.security.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,15 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
 
+   private final AuthenticationService authenticationService;
+
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
-        return null;
+    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+        return authenticationService.register(registerRequest);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-        return null;
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        return authenticationService.login(loginRequest);
     }
 }
