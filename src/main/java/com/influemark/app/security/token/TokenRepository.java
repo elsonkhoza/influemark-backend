4a181dd4ad36c697452c20cc5744203cceea5d1a
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-10-15 16:34:26 +0200

    added TokenRepository.findAllActiveTokensByInfluencer

--- src/main/java/com/influemark/app/security/token/TokenRepository.java
+++ src/main/java/com/influemark/app/security/token/TokenRepository.java
@@ -1,12 +1,22 @@
 package com.influemark.app.security.token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByToken(String token);
+
+    @Query("select t from Token t where t.influencer.id = ?1 " +
+            "and (t.expired = false or t.revoked = false)")
+    List<Token> findAllActiveTokensByInfluencer(Long id);
+
+
+
+
 
 }
