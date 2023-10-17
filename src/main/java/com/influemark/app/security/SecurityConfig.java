5378aa47f7f07d4050f5d145ef509787cb7ea1f8
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-10-17 20:33:47 +0200

    SecurityConfig.securityFilterChain update

--- src/main/java/com/influemark/app/security/SecurityConfig.java
+++ src/main/java/com/influemark/app/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.influemark.app.security;
 
 import com.influemark.app.influencer.InfluencerUserDetailsService;
+import com.influemark.app.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +10,22 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.security.config.http.MatcherType.mvc;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 
 @Configuration
 @EnableWebSecurity
@@ -21,8 +33,30 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final InfluencerUserDetailsService influencerUserDetailsService;
+   private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
+                                                   HandlerMappingIntrospector handlerMappingIntrospector
+                                                   ) throws Exception {
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(handlerMappingIntrospector);
+
+        httpSecurity.
+                csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth->auth
+                                .requestMatchers(mvcMatcherBuilder.pattern("/api/v1/auth/**"))
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+
+                )
+                .sessionManagement(
+                        session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return null;
     }
 
